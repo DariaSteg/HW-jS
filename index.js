9c04bc323f72be8b9bec451e876c7a4d36b73c13
DariaSteg <dariasteg@gmail.com>
2022-11-06 22:23:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let age;
+let ifadult;
+let programlanguage;
+let ifhavePet;
+
+age = '23';
+ifadult = "true";
+programlanguage = 'C++';
+ifhavePet = undefined;
+console.log(age)
+console.log(ifadult)
+console.log(programlanguage)
+console.log(ifhavePet)
